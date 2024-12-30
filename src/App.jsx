d364ea415dcbef4d7e7aab4e098dbd5a657ea688
ArunKumar1007 <154604708+GitArunjangid@users.noreply.github.com>
2024-12-30 20:12:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Paste from "./components/paste";
+import Paste from "./components/Paste";
 import ViewPaste from "./components/ViewPastes";
 import { Toaster } from "react-hot-toast";
 
